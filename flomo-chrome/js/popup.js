e2414e36f64abae935172ddda3d91a0a69dc5d30
extrastu <1067633131@qq.com>
2020-12-30 16:21:09 +0800

    优化popup
    (cherry picked from commit 410e1001c888c36eb51bb0f6075b946c6d81c49d)

--- flomo-chrome/js/popup.js
+++ flomo-chrome/js/popup.js
@@ -1,13 +1,4 @@
 (function() {
-    var api = localStorage.api || '';
-
-    if (api) {
-        $('#tip').text('已经配置API，可以愉快的send memo to flomo了')
-    } else {
-        $('#tip').text('右键flomo icon 选择选项，进入选项页填写flomo API')
-    }
-
-
     document.addEventListener('DOMContentLoaded', function() {
         for (const anchor of document.getElementsByTagName('a')) {
             anchor.onclick = () => {
@@ -15,4 +6,5 @@
             };
         };
     });
+    
 })();
